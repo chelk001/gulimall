bcd15d7e73dd29e84872fbdbc8eaada94be14e7e
chelk <346309573@qq.com>
2020-05-29 00:06:04 +0800

    第三次次提交

--- gulimall-coupon/src/main/java/com/atgugiu/gulimall/coupon/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atgugiu/gulimall/coupon/coupon/GulimallCouponApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GulimallCouponApplication {
-
+    //启动入口
     public static void main(String[] args) {
         SpringApplication.run(GulimallCouponApplication.class, args);
     }
